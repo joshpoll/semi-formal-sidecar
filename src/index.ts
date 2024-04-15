c78fc1fe0c12c188442cabfe66e790870c799e9a
Josh Pollock <jopo@mit.edu>
2024-04-15 11:20:20 -0400

    astronomy widget

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 /**
@@ -10,22 +14,67 @@ import { ISettingRegistry } from '@jupyterlab/settingregistry';
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'semi-formal-sidecar:plugin',
+  requires: [ICommandPalette],
   description: 'A JupyterLab extension.',
   autoStart: true,
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
-    console.log('JupyterLab extension semi-formal-sidecar is activated!');
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    settingRegistry: ISettingRegistry | null
+  ) => {
+    console.log('JupyterLab extension jupyterlab_apod is activated!');
 
     if (settingRegistry) {
       settingRegistry
         .load(plugin.id)
         .then(settings => {
-          console.log('semi-formal-sidecar settings loaded:', settings.composite);
+          console.log(
+            'semi-formal-sidecar settings loaded:',
+            settings.composite
+          );
         })
         .catch(reason => {
-          console.error('Failed to load settings for semi-formal-sidecar.', reason);
+          console.error(
+            'Failed to load settings for semi-formal-sidecar.',
+            reason
+          );
         });
     }
+
+    // Define a widget creator function,
+    // then call it to make a new widget
+    const newWidget = () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = new Widget();
+      const widget = new MainAreaWidget({ content });
+      widget.id = 'apod-jupyterlab';
+      widget.title.label = 'Astronomy Picture';
+      widget.title.closable = true;
+      return widget;
+    };
+    let widget = newWidget();
+
+    // Add an application command
+    const command: string = 'apod:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: () => {
+        // Regenerate the widget if disposed
+        if (widget.isDisposed) {
+          widget = newWidget();
+        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
